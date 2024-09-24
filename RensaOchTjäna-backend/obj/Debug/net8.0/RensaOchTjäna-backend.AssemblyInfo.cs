f7d04b5526254e105388152b4e9bd2d8d183baf0
Delzarkafashi <Delzarkafashi@gmail.com>
2024-09-24 12:05:26 +0200

    uppdatering

--- RensaOchTjäna-backend/obj/Debug/net8.0/RensaOchTjäna-backend.AssemblyInfo.cs
+++ RensaOchTjäna-backend/obj/Debug/net8.0/RensaOchTjäna-backend.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RensaOchTjäna-backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d6a82bc2a1f6ee7a9b840bdb3eadb9ec96847808")]
 [assembly: System.Reflection.AssemblyProductAttribute("RensaOchTjäna-backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RensaOchTjäna-backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
